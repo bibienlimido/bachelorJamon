44b69911cfe03ee3651c576f5c6372e5f8bef1ae
freyer <bibien.limido@Freyair.local>
2015-03-12 10:39:29 +0100

    v1

--- hello.js
+++ hello.js
@@ -2,6 +2,8 @@
 var http = require('http');
 /*Demande l'url*/
 var url = require("url");
+/*recupère des parametres*/
+var queryString = require('queryString');
 
 /*Le serveur repond 200 et un html*/
 var server = http.createServer(function(req, res) {
@@ -9,17 +11,28 @@ var server = http.createServer(function(req, res) {
 	var page = url.parse(req.url).pathname;
 	/*log dans la console*/
 	console.log(page);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    if(page == "/"){
-    	res.write('Vous etes a l\'accueil , que puis-je pour vour');
-    	
-    }
-    else if(page == '/index'){
-    	res.write('vous etes sur l\'index');
-    	
-    }
+	var params = querystring.parse(url.parse(req.url).query);
+	res.writeHead(200, {
+		"Content-Type": "text/html"
+	});
+	if (page == "/") {
+		res.write('Vous etes a l\'accueil , que puis-je pour vour');
 
-    res.end();
+	} else if (page == '/index') {
+		res.write('vous etes sur l\'index');
+
+		if ('prenom' in params && 'nom' in params) {
+			res.write('Vous vous appelez ' + params['prenom'] + ' ' + params['nom']);
+		} else {
+			res.write('Vous devez bien avoir un prénom et un nom, non ?');
+		}
+
+
+	}
+
+
+
+	res.end();
 });
 /*port d'écoute du serveur*/
 server.listen(8080);
