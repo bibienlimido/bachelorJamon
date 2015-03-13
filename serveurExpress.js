771ff59f04514becfd2da78c22ea05b0650061c4
freyer <bibien.limido@157.26.104.136>
2015-03-13 11:26:06 +0100

    ServeurExpress.js

--- serveurExpress.js
+++ serveurExpress.js
@@ -1,10 +1,10 @@
 var express = require('express');
-2
-3var app = express();
-4
-5app.get('/', function(req, res) {
-6    res.setHeader('Content-Type', 'text/plain');
-7    res.end('Vous êtes à l\'accueil');
-8});
-9
-10app.listen(8080);
+
+var app = express();
+
+app.get('/', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+app.listen(8080);
