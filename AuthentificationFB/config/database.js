0c561656bd2ea85aa5e0cef1d7b24cbcde01a50b
Bibien Limido <root@Bachelor>
2015-03-14 14:08:26 +0100

    authentification end add authentificationFB

---
+++ AuthentificationFB/config/database.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+// config/database.js
+module.exports = {
+/*
+ * config pour la db
+ */
+    'url' : 'mongodb://authuser:authuser@192.168.1.122:27017/authdb'
+	 // looks like : mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
