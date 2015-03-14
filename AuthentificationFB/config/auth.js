3673e2b6f5a3c9383c07f3c631bc03677467fad2
freyer <bibien.limido@157.26.104.136>
2015-03-14 14:35:46 +0100

    facebook

--- AuthentificationFB/config/auth.js
+++ AuthentificationFB/config/auth.js
@@ -1,7 +1,22 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// expose our config directly to our application using module.exports
+module.exports = {
 
+    'facebookAuth' : {
+        'clientID'      : '1571579233109049', // your App ID
+        'clientSecret'  : '83a8da20315fd831ceca4cca500959f7', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
 
+    'twitterAuth' : {
+        'consumerKey'       : 'your-consumer-key-here',
+        'consumerSecret'    : 'your-client-secret-here',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : 'your-secret-clientID-here',
+        'clientSecret'  : 'your-client-secret-here',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
