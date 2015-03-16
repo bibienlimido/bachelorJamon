6ec255a67b8ec0db81a188029a816e46b6d751d8
freyer <bibien.limido@157.26.104.136>
2015-03-16 13:29:39 +0100

    Stormpath v1

--- stormPach/stormPach.js
+++ stormPach/stormPach.js
@@ -3,7 +3,7 @@ var stormpath = require('express-stormpath');
 
 var app = express();
 app.use(stormpath.init(app, {
-    
+    apiKeyFile: '~/.stormpath/apiKey-3R08K8X4XFJT0B9R1PFDXVNXX.properties',
     application: 'https://api.stormpath.com/v1/applications/3IAxsi6RMbGtFAKMhQhro2',
     secretKey: 'some_long_random_string',
 }));
