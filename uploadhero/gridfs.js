d88366769ee4dc445c13ff6b624f9d3f329d8e0e
freyer <bibien.limido@157.26.108.9>
2015-03-19 18:00:23 +0100

    uh 1

--- uploadhero/gridfs.js
+++ uploadhero/gridfs.js
@@ -1,14 +1,25 @@
 var mongo = require('mongodb');
 var Grid = require('gridfs-stream');
 
-// create or use an existing mongodb-native db instance.
-// for this example we'll just create one:
-var db = new mongo.Db('yourDatabaseName', new mongo.Server("192.168.1.140", 27017));
+// create or use an existing mongodb-native db instance
+var db = new mongo.Db('yourDatabaseName', new mongo.Server("127.0.0.1", 27017));
+var gfs = Grid(db, mongo);
 
-// make sure the db instance is open before passing into `Grid`
-db.open(function (err) {
-  if (err) return handleError(err);
-  var gfs = Grid(db, mongo);
-
-  // all set!
+// streaming to gridfs
+var writestream = gfs.createWriteStream({
+    filename: 'my_file.txt'
 });
+fs.createReadStream('/some/path').pipe(writestream);
+
+// streaming from gridfs
+var readstream = gfs.createReadStream({
+  filename: 'my_file.txt'
+});
+
+//error handling, e.g. file does not exist
+readstream.on('error', function (err) {
+  console.log('An error occurred!', err);
+  throw err;
+});
+
+readstream.pipe(response);
