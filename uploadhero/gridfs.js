31145df21fdaa2c027b785b94677d9cdda9c6721
freyer <bibien.limido@157.26.108.9>
2015-03-19 17:50:46 +0100

    uh 1

---
+++ uploadhero/gridfs.js
@@ -0,0 +1,14 @@
+var mongo = require('mongodb');
+var Grid = require('gridfs-stream');
+
+// create or use an existing mongodb-native db instance.
+// for this example we'll just create one:
+var db = new mongo.Db('yourDatabaseName', new mongo.Server("192.168.1.140", 27017));
+
+// make sure the db instance is open before passing into `Grid`
+db.open(function (err) {
+  if (err) return handleError(err);
+  var gfs = Grid(db, mongo);
+
+  // all set!
+})
