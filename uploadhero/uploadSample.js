46949771cd658468e5c2ff9ff840c511709c252e
freyer <bibien.limido@157.26.108.9>
2015-03-19 16:25:31 +0100

    uh 1

--- uploadhero/uploadSample.js
+++ uploadhero/uploadSample.js
@@ -1,39 +1,17 @@
 /*Define dependencies.*/
+var express = require('express'),
+    multer  = require('multer')
 
-var express = require("express");
-var multer = require('multer');
-var app = express();
-var done = false;
+var app = express()
 
-/*Configure the multer.*/
-
-app.use(multer({dest: './uploads/',
-    rename: function(fieldname, filename) {
-        return filename + Date.now();
-    },
-    onFileUploadStart: function(file) {
-        console.log(file.originalname + ' is starting ...')
-    },
-    onFileUploadComplete: function(file) {
-        console.log(file.fieldname + ' uploaded to  ' + file.path)
-        done = true;
-    }
-}));
-
-/*Handling routes.*/
-
-app.get('/', function(req, res) {
-    res.sendFile("index.html" ,{ root: __dirname });
+app.get('/', function(req, res){
+  res.send('index.html');
 });
 
-app.post('/api/photo', function(req, res) {
-    if (done == true) {
-        console.log(req.files);
-        res.end("File uploaded.");
-    }
-});
+app.post('/',[ multer({ dest: './uploads/'}), function(req, res){
+    console.log(req.body) // form fields
+    console.log(req.files) // form files
+    res.status(204).end()
+}]);
 
-/*Run the server.*/
-app.listen(3000, function() {
-    console.log("Working on port 3000");
-});
+app.listen(3000);
